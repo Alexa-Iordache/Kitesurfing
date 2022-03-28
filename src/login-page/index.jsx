8840324f600352cf6f0ca60b2572497383e42446
Alexa-Iordache <alexa.iordache2013@gmail.com>
2022-03-28 22:32:14 +0300

    dissabled login  button added

--- src/login-page/index.jsx
+++ src/login-page/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,  { useState } from "react";
 import './login-page.css';
 import TextField from "@mui/material/TextField";
 import { useNavigate } from "react-router-dom";
@@ -8,6 +8,20 @@ import { useNavigate } from "react-router-dom";
 export default function Login() {
 
     const navigate = useNavigate();
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+
+    // function to update the text from the 'username' input  
+    function handleChange(e) {
+        setName(e.target.value);
+        // console.log(e.target.value);
+    }; 
+
+      // function to update the text from the 'password' input  
+    function handleChangePassword(e) {
+        setPassword(e.target.value);
+        // console.log(e.target.value);
+    };
 
     return (
         <div className="login__main-container">
@@ -21,7 +35,9 @@ export default function Login() {
                 required
                 id="login__input"
                 label="Username"
-                variant="filled" />
+                variant="filled"
+                value = {name}
+                onChange = {(e) => handleChange(e)} />
 
             {/* description for the second input */}
             <div className="login__password"> Please enter your password:</div>
@@ -34,12 +50,22 @@ export default function Login() {
                 type="password"
                 autoComplete="current-password"
                 variant="filled"
+                value = {password}
+                onChange = {(e) => handleChangePassword(e)}
             />
 
             {/* the login button */}
-            {/* when the user clickes the 'login' button, they are redirected to the 'dashboard' page   */}
             <div>
-                <button className="login__button" onClick={() => {navigate('/dashboard'); }}> Login</button>
+                <button className = "login__button"
+                onClick={() => {navigate('/dashboard'); console.log("buton"); }} // when the user clickes the 'login' button, 
+                                                                        // they are redirected to the 'dashboard' page
+                disabled={name === '' || password === '' ? true : false} // if username or password is not written, 
+                                                                        // then the user can not log in (the 'login' button is disabled) 
+                style={{
+                    backgroundColor: name === '' || password === '' ? 'grey' : 'rgb(79, 144, 230)' // the color of the button will be grey if
+                                                                    //  the button is disabled, and it will turn blue when it is abled 
+                }}> 
+                Login</button>
             </div>
         </div>
     );
