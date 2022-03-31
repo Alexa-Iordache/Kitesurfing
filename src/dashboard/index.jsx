697449ad105e9c166165c7c6a3020eb98b2ac15e
Alexa-Iordache <alexa.iordache2013@gmail.com>
2022-03-31 13:16:23 +0300

    dashboard header added

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import './dashboard.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import axios from "axios";
+// import { iconPerson } from './Icon';
+// import { icon } from "leaflet";
 
 export default class Dashboard extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             spots: [],
-            activeSpot: null
+            activeSpot: null,
         }
     }
 
@@ -23,37 +25,42 @@ export default class Dashboard extends React.Component {
 
     render() {
         return (
-            // there was choosen the center of Bucharest, Romania, as starting point
-            <MapContainer center={[44.4317188182863, 26.102904157147936]} zoom={12} scrollWheelZoom={true}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
+            <div>
+                {/* header for dashboard page */}
+                <div className="dashboard__header">Kite</div>
 
-                {/* there were put markers on every point in the map written in the JSON file */}
-                {this.state.spots.map((spot) => (
-                    <Marker key={spot.id}
-                        position={[spot.lat, spot.long]}
-                        eventHandlers={{    // event handler for click is added on markers
-                            click: () => {
-                                //console.log(spot.lat, spot.long);
-                                this.setState({
-                                    activeSpot: spot    // if a marker is clicked, the spot will be active
-                                })
-                                console.log(this.state.activeSpot);
-                            }
-                        }}
+                {/* there was choosen the center of Bucharest, Romania, as starting point */}
+                <MapContainer className="map-container" center={[44.4317188182863, 26.102904157147936]} zoom={12} scrollWheelZoom={true}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                ))}
 
-                {/* if the spot is active (it was clicked on), then a popup will appear */}
-                {this.state.activeSpot && (
+                    {/* there were put markers on every point in the map written in the JSON file */}
+                    {this.state.spots.map((spot) => (
+                        <Marker key={spot.id}
+                            position={[spot.lat, spot.long]}
+                            eventHandlers={{    // event handler for click is added on markers
+                                click: () => {
+                                    //console.log(spot.lat, spot.long);
+                                    this.setState({
+                                        activeSpot: spot    // if a marker is clicked, the spot will be active
+                                    })
+                                    console.log(this.state.activeSpot);
+                                }
+                            }}
+
+                        />
+                    ))}
+
+                    {/* if the spot is active (it was clicked on), then a popup will appear */}
+                    {/* {this.state.activeSpot && (
                     <Popup position={[this.state.activeSpot.lat, this.state.activeSpot.long]}>
                         <div>popup</div>
                     </Popup>
-                )}
-
-            </MapContainer>
+                )} */}
+                </MapContainer>
+            </div>
         );
     }
 }
